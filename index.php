c3fbe87d85ae0676b367dc2eff86dc1592965808
EdiPoi <eddiepoirier03@gmail.com>
2025-06-09 16:43:20 -0400

    initial

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Kenny Note Thing</title>
+
+    <style>
+        .body{background-color:#000;}
+    </style>
+</head>
+<body>
+    <h1>Kenny Note Thing</h1>
+</body>
+</html>
